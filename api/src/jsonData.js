07c72298a21d956b2ad81fb0578f938ecdf547ad
Renato Uzcategui <renauzca2103@gmail.com>
2022-08-16 13:18:06 -0300

    subiendo el backend

---
+++ api/src/jsonData.js
@@ -0,0 +1,90 @@
+module.exports({
+  panes: [
+    {
+      name: "Pan de trigo",
+      description:
+        "La harina de trigo es la más utilizada para la elaboración de pan. De hecho, algunos panes elaborados con otras harinas suelen llevar también algún porcentaje de trigo. Lo podemos tomar blanco o integral. La principal diferencia en el proceso de elaboración del pan blanco y el integral es que para este último se utiliza harina sin refinar, lo que hace que este tipo de pan tenga un mayor valor nutritivo, siendo más rico en fibra, vitaminas y minerales. Además, al tener más fibra fomenta una mayor sensación de saciedad. No obstante, la cantidad de calorías aportadas en ambos casos suele ser muy similar. 100 g de pan blanco tienen unas 260 kcal frente a las 230 kcal del pan integral.",
+      price: "$1.00",
+      image: "https://inmendoza.com/wp-content/uploads/2019/08/alfajores4.jpg",
+      type: "pan" // pan, facturas, sandwiches, bebidas, postres
+    },
+    {
+      name: "Pan de maíz",
+      description:
+        "Se elabora con harina de maíz que no contiene gluten, lo que hace que sea un pan apto para celíacos.  Además es bajo en purinas, por lo que es un alimento que también pueden tomar sin problemas las personas con el acido úrico alto.",
+      price: "$2.00",
+      image: "https://img.cocinarico.es/2020-07/pan-de-maiz-1.jpg",
+      type: "pan"
+    },
+    {
+      name: "Croissant",
+      description:
+        "Harina con germen, margarina, agua, harina integral, masa madre, azúcar de caña, gluten, sal, levadura prensada",
+      price: "$3.00",
+      image:
+        "https://www.clarin.com/img/2021/01/25/CsjOcZxC9_1256x620__2.jpg#1611582039092",
+        type: "pan"
+    },
+    {
+      name: "Palmera con chocolate",
+      description:
+        "Masa hojaldre, azúcar de caña, chocolate, manteca de cacao, granillo de almendra, huevos.",
+      price: "$4.00",
+      image:
+        "https://badun.nestle.es/imgserver/v1/80/1290x742/palmeras-de-chocolate.jpg",
+        type: "pan"
+    },
+    {
+      name: "Galletas de miel y naranja",
+      description:
+        "Harina con germen, grasa de palma, azúcar de panela, harina integral, leche en polvo, agua, miel, bicarbonato, esencia de naranja, sal.",
+      price: "$5.00",
+      image: "https://i.ytimg.com/vi/Qv138vFiiRY/maxresdefault.jpg",
+      type: "pan"
+    },
+    {
+      name: "Galletas de coco",
+      description:
+        "Harina con germen, huevos, azúcar de caña, coco rallado, mantequilla, harina de almendra, grasa de palma.",
+      price: "$6.00",
+      image:
+        "https://www.comidastipicaschilenas.com/wp-content/uploads/2021/06/Receta-de-galletas-de-coco-caseras.jpg",
+        type: "pan"
+    },
+    {
+      name: "Baguette",
+      description:
+        "El origen de la baguette se remonta a los años 1830, cuando se introdujo el pan vienés en Francia. Se trataba de un pan alargado, fermentado con levadura de cerveza que, al ser cocido en un horno de vapor, adquiría una corteza crujiente y una miga blanca y esponjosa.",
+      price: "$7.00",
+      image:
+        "https://img.freepik.com/foto-gratis/rebanada-pan-baguette-sobre-mantel_114579-5829.jpg?w=2000",
+        type: "pan"
+    }, 
+    {
+      name: "Muffins",
+      description:
+        "Producto de repostería elaborado con pan dulce y otros ingredientes. Se cocina al horno en moldes individuales, presentan una base cilíndrica y una copa más ancha en la parte superior.",
+      price: "$8.00",
+      image:
+        "https://www.recetasderechupete.com/wp-content/uploads/2020/02/Muffins-de-frambuesa.jpg",
+        type: "pan"
+    },
+    {
+      name: "Brownies",
+      description:
+        "es un bizcocho de chocolate que contiene nueces y que se cuece al horno sobre una placa. Gracias a su índice elevado de azúcar y mantequilla, presenta una textura particular: crujiente por arriba y muy tierna en su interior. Se sirve cortado en cuadrados y a menudo se acompaña con crema.",
+      price: "$9.00",
+      image:
+        "https://scm-assets.constant.co/scm/unilever/2bb5223be0548fcc55c230aa5f951219/ca8ed1c9-8dc4-43f6-9331-f23c444d2314.png",
+        type: "pan"
+    },
+    {
+      name: "Alfajor",
+      description:
+        "Esta variedad se compone de dos o más galletas unidas por un relleno dulce y generalmente bañadas en chocolate, glaseado o azúcar en polvo. El relleno puede ser de dulce de leche o miel, aunque también existen alfajores de frutas, mousse de chocolate y diferentes rellenos.",
+      price: "$10.00",
+      image: "https://pampadirect.com/product_images/uploaded_images/alfajor-negro-relleno-de-dulce-de-leche.jpg",
+      type: "pan"
+    },
+  ],
+});
