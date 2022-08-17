5285f5d6a0c905059816ac5be2bb67d31a5514ee
Diego0014 <88014955+Diego0014@users.noreply.github.com>
2022-08-17 19:20:16 -0300

    Creando SEEDER para agregar productos a BDD

--- api/src/jsonData.js
+++ api/src/jsonData.js
@@ -1,4 +1,4 @@
-module.exports({
+module.exports = {
   panes: [
     {
       name: "Pan de trigo",
@@ -228,4 +228,4 @@ module.exports({
     }
 
   ],
-});
+};
--- api/src/routes/Products/index.js
+++ api/src/routes/Products/index.js
@@ -1,13 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 
-
-
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 
 module.exports = router;
