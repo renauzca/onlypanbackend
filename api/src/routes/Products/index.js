4f76acaf2206712e2b47f4a7c3c9d121f149e33f
GuerreroEnier <guerreroenier@gmail.com>
2022-08-17 15:45:17 -0500

    propando que no se pise

--- api/src/routes/Products/index.js
+++ api/src/routes/Products/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const {getProducts} = require('./getProducts')
 
 
 
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get('/', getProducts);
 
 
 module.exports = router;
