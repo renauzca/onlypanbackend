24af0426164ca3ab928e49d3c7cedba0009e4f71
GuerreroEnier <guerreroenier@gmail.com>
2022-08-17 08:29:31 -0500

    actualizando JSON de OnlyPan

--- api/src/jsonData.js
+++ api/src/jsonData.js
@@ -6,7 +6,7 @@ module.exports({
         "La harina de trigo es la más utilizada para la elaboración de pan. De hecho, algunos panes elaborados con otras harinas suelen llevar también algún porcentaje de trigo. Lo podemos tomar blanco o integral. La principal diferencia en el proceso de elaboración del pan blanco y el integral es que para este último se utiliza harina sin refinar, lo que hace que este tipo de pan tenga un mayor valor nutritivo, siendo más rico en fibra, vitaminas y minerales. Además, al tener más fibra fomenta una mayor sensación de saciedad. No obstante, la cantidad de calorías aportadas en ambos casos suele ser muy similar. 100 g de pan blanco tienen unas 260 kcal frente a las 230 kcal del pan integral.",
       price: "$1.00",
       image: "https://inmendoza.com/wp-content/uploads/2019/08/alfajores4.jpg",
-      type: "pan" // pan, facturas, sandwiches, bebidas, postres
+      type: "pan" 
     },
     {
       name: "Pan de maíz",
@@ -32,7 +32,7 @@ module.exports({
       price: "$4.00",
       image:
         "https://badun.nestle.es/imgserver/v1/80/1290x742/palmeras-de-chocolate.jpg",
-        type: "pan"
+        type: "factura"
     },
     {
       name: "Galletas de miel y naranja",
@@ -40,7 +40,7 @@ module.exports({
         "Harina con germen, grasa de palma, azúcar de panela, harina integral, leche en polvo, agua, miel, bicarbonato, esencia de naranja, sal.",
       price: "$5.00",
       image: "https://i.ytimg.com/vi/Qv138vFiiRY/maxresdefault.jpg",
-      type: "pan"
+      type: "galleta"
     },
     {
       name: "Galletas de coco",
@@ -49,7 +49,7 @@ module.exports({
       price: "$6.00",
       image:
         "https://www.comidastipicaschilenas.com/wp-content/uploads/2021/06/Receta-de-galletas-de-coco-caseras.jpg",
-        type: "pan"
+        type: "galleta"
     },
     {
       name: "Baguette",
@@ -67,7 +67,7 @@ module.exports({
       price: "$8.00",
       image:
         "https://www.recetasderechupete.com/wp-content/uploads/2020/02/Muffins-de-frambuesa.jpg",
-        type: "pan"
+        type: "reposteria"
     },
     {
       name: "Brownies",
@@ -76,7 +76,7 @@ module.exports({
       price: "$9.00",
       image:
         "https://scm-assets.constant.co/scm/unilever/2bb5223be0548fcc55c230aa5f951219/ca8ed1c9-8dc4-43f6-9331-f23c444d2314.png",
-        type: "pan"
+        type: "reposteria"
     },
     {
       name: "Alfajor",
@@ -84,7 +84,148 @@ module.exports({
         "Esta variedad se compone de dos o más galletas unidas por un relleno dulce y generalmente bañadas en chocolate, glaseado o azúcar en polvo. El relleno puede ser de dulce de leche o miel, aunque también existen alfajores de frutas, mousse de chocolate y diferentes rellenos.",
       price: "$10.00",
       image: "https://pampadirect.com/product_images/uploaded_images/alfajor-negro-relleno-de-dulce-de-leche.jpg",
-      type: "pan"
+      type: "reposteria"
     },
+    {
+      name: "medialunas de grasa",
+      description:"Clasico argentino, que viene bien acompana de un cafe. Estan hechas con grasa y margarina",
+      price:"4.00",
+      image:"https://cocinerosargentinos.com/content/recipes/500x500/recipes.8952.jpeg",
+      type:"factura"
+    },
+    {
+      name: "medialunas de manteca",
+      description:"Esta es una segunda opcion a las de grasa, muchos se pelean entre decidir cual de las dos es la mejor, pero esta se realiza con manteca, un toque de esencia de vainilla y cubiertas de una capa de almibar, los cuales las hacen mas dulce.",
+      price:"5.00",
+      image:"https://st2.depositphotos.com/4278641/8937/i/600/depositphotos_89375034-stock-photo-tasty-fresh-croissants.jpg",
+      type:"factura"
+    },
+    {
+      name: "Cheesecake de Limon",
+      description:"Suave pionono de vainilla, batido de queso crema, jugo y rayadura de limón. Cobertura: geleé de frutos rojos",
+      price:"30.00",
+      image:"https://www.nuria.com.ar/wp-content/uploads/2018/12/Confiteria-Nuria-2018-141.jpg",
+      type:"reposteria"
+    },
+    {
+      name: "Tres Leches",
+      description:"Torta de vainilla humedecida con leche condensada, crema chantilly y abundante dulce de leche. Cobertura: fina capa de merengue italiano.",
+      price:"25.00",
+      image:"https://www.nuria.com.ar/wp-content/uploads/2018/11/Confiteria-Nuria-2018-40.jpg",
+      type:"reposteria"
+    },
+    {
+      name: "Sandwiches de Miga",
+      description:"Es un tipo de sándwich característico de la gastronomía argentina, paraguaya, uruguaya y peruana. Se trata de un emparedado cuyas tapas o paredes están compuestas por finas secciones del llamado pan de miga o pan de molde.",
+      price:"10.00",
+      image:"https://assets.iprofesional.com/cdn-cgi/image/w=880,f=webp/https://assets.iprofesional.com/assets/jpg/2021/02/512110.jpg",
+      type:"sandwiche"
+    },
+    {
+      name: "Masas de Secas",
+      description:"Alfajorcitos de maicena, de chocolate, de nuez, de naranja, hojitas de limón, pepas de membrillo, pepas de chocolate negro.",
+      price:"2.00",
+      image:"https://www.nuria.com.ar/wp-content/uploads/2018/12/Confiteria-Nuria-2018-264.jpg",
+      type:"galleta"
+    },
+    {
+      name: "Lemon Pie",
+      description:"Base de masa sablée, crema de limón. Cobertura: merengue italiano.",
+      price:"30.00",
+      image:"https://www.nuria.com.ar/wp-content/uploads/2018/12/Confiteria-Nuria-2018-58.jpg",
+      type:"reposteria"
+    },
+    {
+      name: "Coca Cola",
+      description:"Bebida refrescante",
+      price:"7.00",
+      image:"https://sancayetano.com.py/wp-content/uploads/2021/01/11840.jpg",
+      type:"bebida"
+    },
+    {
+      name: "Seven Up",
+      description:"Bebida refrescante",
+      price:"7.00",
+      image:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3080040_f.jpg",
+      type:"bebida"
+    },
+    {
+      name: "Fanta",
+      description:"Bebida refrescante",
+      price:"7.00",
+      image:"https://lachimbotana.com.ar/wp-content/uploads/2020/06/Fanta500ml.png",
+      type:"bebida"
+    },
+    {
+      name: "Pepsi",
+      description:"Bebida refrescante",
+      price:"7.00",
+      image:"https://greenboom.com.ar/wp-content/uploads/2020/11/pepsi.jpg",
+      type:"bebida"
+    },
+    {
+      name: "Mocca",
+      description:"Es una variante del café con leche. Como este, suele llevar un tercio de expreso y dos tercios de leche vaporizada, pero se añade una parte de chocolate, normalmente en forma de jarabe de chocolate",
+      price:"15.00",
+      image:"https://perfectdailygrind.com/wp-content/uploads/2020/09/Mocha-4.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Latte",
+      description:"Es el hermano mayor del capuchino, donde todo gira en torno a la espuma suave y sedosa. Normalmente, el café latte se prepara con una o dos dosis de expreso (1/3 de la bebida) y 2/3 de leche calentada con vapor, con una pequeña capa (alrededor de 1 cm) de espuma de leche.",
+      price:"18.00",
+      image:"https://planaxia.com/wp-content/uploads/2020/07/latte.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Espresso",
+      description:"es un tipo de café negro fuerte que se forma cuando el agua caliente pasa a mucha presión través de los granos de café molidos. Si bien esto puede sonar similar a un café estándar, son muy diferentes. El espresso requiere mucha más presión y mucho menos tiempo de preparación que el café.",
+      price:"21.00",
+      image:"https://dam.cocinafacil.com.mx/wp-content/uploads/2013/04/cafe-espresso.jpg",
+      type:"cafe",
+    },
+    {
+      name: "Americano",
+      description:"es la bebida caliente ideal para las personas que quieren tomar un delicioso espresso, pero que no toleran el sabor tan intenso del espresso. Porque el americano no es otra cosa que un espresso con agua caliente, o más exactamente: un café que consiste en partes exactamente iguales de espresso y agua.",
+      price:"12.00",
+      image:"https://www.lajoyadelcafe.com/wp-content/uploads/2017/12/Caf%C3%A9-americano-taza-e1621711044716.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Cappuccino",
+      description:"es una bebida nacida en Italia, preparada con café expreso y leche montada con vapor para darle cremosidad. Un capuchino se compone de 125 ml de leche y 25 ml de café expreso, en ocasiones se agrega cacao en polvo o canela según el gusto del consumidor.",
+      price:"25.00",
+      image:"https://todocafe.es/wp-content/uploads/2020/04/Cafe_capuchino.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Machiatto",
+      description:"en italiano, también llamado espresso macchiato, es un café cortado típico de Italia, consiste en un expreso con una pequeña cantidad de leche caliente y espumada. En España se suele denominar café cortado.",
+      price:"24.00",
+      image:"https://upload.wikimedia.org/wikipedia/commons/e/e3/Macchiato_FourBarrel.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Cafe con leche",
+      description:"Es una mezcla de espuma de leche y el líquido de la leche vaporizada. El café con leche es un clásico entre los consumidores de café que prefieren un sabor de expreso más fuerte. La leche en el café con leche aporta o mejora el sabor de la bebida, aunque el expreso sigue siendo el sabor dominante en la boca.",
+      price:"15.00",
+      image:"https://incapto.com/wp-content/uploads/2020/11/Coffee-of-milk.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Frappe",
+      description:"es un café con hielo cubierto de espuma elaborado a partir de café instantáneo. Es muy popular en Grecia y Chipre, especialmente durante el verano, si bien se ha extendido a otros países.",
+      price:"30.00",
+      image:"https://www.pequerecetas.com/wp-content/uploads/2020/08/frapuccino-starbucks-receta-frappe.jpg",
+      type:"cafe"
+    },
+    {
+      name: "Bizcochos de Grasa",
+      description:"van bien a toda hora. Te pinta el hambre: ahí tenés los bizcochitos de grasa. Media mañana o media noche, da igual. Los bizcochos de grasa versátiles en cuanto a horario se refiere. Ni hablar del desayuno y la merienda porque el bizcochito es el rey.",
+      price:"3.00",
+      image:"https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1024/http://recetas-caseras.info/wp-content/uploads/2017/08/Bizcochos-de-grasa.jpg",
+      type:"factura"
+    }
+
   ],
 });
