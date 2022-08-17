54c9bd7a18efcda7e32d7b7bfa1adca298b323ae
FacuF54 <facu_fernandez54@hotmail.com>
2022-08-17 18:55:09 -0300

    sssss

--- api/src/app.js
+++ api/src/app.js
@@ -21,7 +21,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
+}); 
 
 server.use('/product', routeProduct);
 server.use('/user', routeUser);
