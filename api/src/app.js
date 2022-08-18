6f74f72e5091a4d26641e2d5fbb56c393e0065e1
FacuF54 <facu_fernandez54@hotmail.com>
2022-08-18 12:16:45 -0300

    subiendo sin env

--- api/src/app.js
+++ api/src/app.js
@@ -24,7 +24,7 @@ server.use((req, res, next) => {
 });
 
 
-server.use('/product', routeProduct); 
+server.use('/product', routeProduct); //estas son las que van
 server.use('/user', routeUser);
 
 
