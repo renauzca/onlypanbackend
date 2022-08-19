94a9a6ce18de45e52417c6669365094e053da90d
GuerreroEnier <guerreroenier@gmail.com>
2022-08-18 22:05:19 -0500

    creacion del endpoint para el eliminado logico del producto

--- api/src/routes/Products/index.js
+++ api/src/routes/Products/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const getProduct = require("./ShearchProduct")
-const postProduct = require("./postProduct")
+const getProduct = require("./ShearchProduct");
+const postProduct = require("./postProduct");
+const deleteProduct = require("./deleteProduct");
 
-router.get("/", getProduct.buscar)
-router.get("/:id", getProduct.id)
-router.post("/", postProduct.crear)
+router.get("/", getProduct.buscar);
+router.get("/:id", getProduct.id);
+router.post("/", postProduct.crear);
+router.delete("/:id", deleteProduct.deleteProduct);
 
 module.exports = router;
