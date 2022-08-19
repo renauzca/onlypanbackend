ef856d41407c49b6402560b41159b5b32186bd45
GuerreroEnier <guerreroenier@gmail.com>
2022-08-18 23:45:24 -0500

    Creación del filtrado por tipo

---
+++ api/src/routes/Products/filterByType.js
@@ -0,0 +1,22 @@
+const {Product} = require('../../db');
+
+
+const filterByType = async(req, res) =>{
+    const {name} = req.query;
+    console.log(name)
+    try {
+        const allProducts = await Product.findAll({
+            where:{type:name}
+        });    
+        
+        allProducts.length
+        ?res.status(200).send(allProducts)
+        :res.status(200).send('No se encontraron productos con este tipo');
+    } catch (error) {
+        throw new Error(error + ' error al momento de filtrar por tipo')
+    }
+
+}
+
+module.exports={filterByType}
+
--- api/src/routes/Products/index.js
+++ api/src/routes/Products/index.js
@@ -4,7 +4,9 @@ const router = Router();
 const getProduct = require("./ShearchProduct");
 const postProduct = require("./postProduct");
 const deleteProduct = require("./deleteProduct");
+const filterByType = require("./filterByType");
 
+router.get("/type", filterByType.filterByType)
 router.get("/", getProduct.buscar);
 router.get("/:id", getProduct.id);
 router.post("/", postProduct.crear);
