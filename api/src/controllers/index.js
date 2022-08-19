14a62487ae18ab20874bc2c880ea4f14aa5031df
GuerreroEnier <guerreroenier@gmail.com>
2022-08-18 23:07:20 -0500

    modificación de controller para evitar que se llene la BD

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -4,6 +4,9 @@ const { Product } = require("../db");
 
 const addProductDB = async () => {
   try {
+    /* Para evitar que se siga llenando la BD*/
+    //let product = await Product.findAll();
+    //!product.length&&await Product.bulkCreate(json.panes);
     await Product.bulkCreate(json.panes);
   } catch (error) {
     console.log(error.message);
