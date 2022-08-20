23fb72d0da6b949ab3a8f80d86056b42bf7fbf52
FacuF54 <82850091+FacuF54@users.noreply.github.com>
2022-08-20 19:02:55 -0300

    ruta filtro por precio y tipo lista

--- api/src/routes/Products/index.js
+++ api/src/routes/Products/index.js
@@ -6,12 +6,15 @@ const postProduct = require("./postProduct");
 const deleteProduct = require("./deleteProduct");
 const filterByType = require("./filterByType");
 const filterByPrice = require("./filterByPrice");
+const filterbyTypePrices = require("./filterbyTypePrice");
 
 router.get("/price", filterByPrice.filterByPrice)
 router.get("/type", filterByType.filterByType)
+router.get("/typ", filterbyTypePrices.filterbyTypePrice)
 router.get("/", getProduct.buscar);
 router.get("/:id", getProduct.id);
 router.post("/", postProduct.crear);
 router.put("/delete/:id", deleteProduct.deleteProduct);
+
 
 module.exports = router;
