ec2635a5659c46cb637aa0ab3fd137c13fc3003f
FacuF54 <82850091+FacuF54@users.noreply.github.com>
2022-08-24 18:52:08 -0300

    lo mismo

--- api/src/routes/type/getTypes.js
+++ api/src/routes/type/getTypes.js
@@ -3,12 +3,14 @@ const { Product } = require("../../db");
 const getTypes = async (req, res) => {
   try {
     const product = await Product.findAll();
-    const productType = product.map(t=> t.type)
+    const productType = product.map(t=> t.dataValues.type)
     const allProduct = new Set(productType)
     let all = []
+    
     for (const item of allProduct) {
       all.push(item)
     }
+    
     res.send(all)
   } catch (error) {
     res.status(404).send(error)
