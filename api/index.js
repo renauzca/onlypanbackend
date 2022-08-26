2ab41b46705529ff13edfaefd044f3050395c92c
FacuF54 <82850091+FacuF54@users.noreply.github.com>
2022-08-26 12:54:28 -0300

    validaciones listas

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { addProductDB } = require("./src/controllers/index.js");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, async () => {
     await addProductDB();
     console.log("%s listening at 3001");
--- api/src/routes/User/signUp.js
+++ api/src/routes/User/signUp.js
@@ -4,12 +4,13 @@ const jwt = require("jsonwebtoken");
 const auth = require("./auth");
 
 const signUp = (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, rol } = req.body;
   if (password.length >= 4) {
     let pass = bcrypt.hashSync(password, Number.parseInt(auth.rounds));
     User.create({
       password: pass,
       email: email,
+      rol:rol
     })
       .then((user) => {
         let token = jwt.sign({ user: user }, auth.secret, {
