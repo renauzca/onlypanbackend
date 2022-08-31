b698cdb58ad741d620fbe72aca885bd7e8a6d679
Braian Chavez <chavezbraian1999@gmail.com>
2022-08-31 10:07:35 -0300

    updateUser

--- api/src/routes/User/index.js
+++ api/src/routes/User/index.js
@@ -5,6 +5,7 @@ const { google } = require("./google");
 const { validate } = require("./policy");
 const { signIn } = require("./signIn");
 const { signUp } = require("./signUp");
+const { updateUser } = require("./updateUserbyUser");
 
 const router = Router();
 
@@ -14,6 +15,7 @@ router.post("/signIn", signIn);
 router.post("/signUp", signUp);
 router.post("/google", google);
 
+router.post("/modify/:id",validate,updateUser);
 router.put("/delete/:id", validate, deleteUser);
 
 module.exports = router;
---
+++ api/src/routes/User/updateUserbyUser.js
@@ -0,0 +1,30 @@
+const { User } = require("../../db");
+
+const updateUser = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const {name , lastName,address} = req.body;
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      res.status(404).send("Usuario no encontrado");
+    } else {
+      await User.update(
+        {
+          name: name,
+          lastName: lastName,
+          address: address,
+          phone: phone,
+        },
+        {
+          where: { id: id },
+        }
+      );
+      res.send("Usuario modificado correctamente!");
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Error al modificar", error });
+  }
+};
+
+module.exports = { updateUser };
