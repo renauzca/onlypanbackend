28832f5b009a410d708494215f74ff072a87bd17
Feli Caeiro <felicaeiro2@gmail.com>
2022-09-03 20:47:22 -0300

    cart

--- api/src/routes/Cart/cart.js
+++ api/src/routes/Cart/cart.js
@@ -1,104 +1,121 @@
-const {Cart, User, Product, ProductCart} = require('../../db');
+const { Cart, User, Product, ProductCart } = require('../../db');
 
-const cart = async(req, res) => {
-    try{
-        console.log()
-        const newCart = await Cart.create();
-        const user = await User.findByPk(req.body.userId);
-        await newCart.setUser(user);
+const getCart = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userCart = await Cart.findAll({
+      where: { userId: id },
+      include: {
+        model: Product,
+        attributes: ['name'],
+        through: { attributes: ['quantity', 'totalPrice'] },
+      },
+    });
+    res.send(userCart);
+  } catch (error) {
+    throw new Error(error + ' Error al obtener el cart');
+  }
+};
 
-        req.body.idProducts.forEach(async (pro)=>{
-            const idProd = await Product.findByPk(pro.id);
-            newCart.addProduct(idProd, {through:{
-                quantity: pro.quantity,
-                totalPrice: pro.totalPrice
-            }})
-        })
+const createCart = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findUserCart = await Cart.findOne({ where: { userId: id } });
+    if (!findUserCart) {
+      const newCart = await Cart.create();
+      const user = await User.findByPk(id);
+      await newCart.setUser(user);
+      if (req.body.length) {
+        req.body.forEach(async (pro) => {
+          await Product.findByPk(pro.id).then((response) =>
+            newCart.addProduct(response, {
+              through: {
+                quantity: pro.quantitySelectedCartSh,
+                totalPrice: pro.quantitySelectedCartSh * pro.price,
+              },
+            })
+          );
+        });
+      }
+      res.send('carrito creado');
+    } else res.status(200).send('El usuario ya tiene un carrito');
+  } catch (error) {
+    console.log(error);
+    throw new Error(error + ' Error al llenar tabla de cart');
+  }
+};
 
-        res.status(200).send('Agregado a la tabla de carrito')
+const updateCart = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-    }catch(error){
-        throw new Error(error +' Error al llenar tabla de cart')
+    await Cart.findOne({ where: { userId: id } }).then(async (cart) => {
+      let pro = await ProductCart.findOne({
+        where: {
+          cartId: cart.id,
+          productId: req.body.id,
+        },
+      });
+
+      if (pro) {
+        await pro.update({
+          quantity: req.body.quantity,
+          totalPrice: req.body.totalPrice,
+        });
+        pro.save();
+        res.status(200).send('Producto Modificado');
+      } else {
+        res.status(500).send('Producto no encontrado');
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error(error + ' Error al cambiar datos del carrito');
+  }
+};
+
+const deleteCart = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let cart = await Cart.findOne({ where: { userId: id } });
+    if (cart) {
+      cart.destroy({ where: { userId: id } });
+      res.status(200).send('Borrado con exito');
+    } else {
+      res.status(500).send('No se encontro un usuario con ese ID');
     }
-}
+  } catch (error) {
+    throw new Error(error + ' error al eliminar');
+  }
+};
 
-const updateCart = async(req, res) => {
-    try{
-        const {id} = req.params;
-        
-        await Cart.findOne({where:{userId:id}})
-            
-            .then(async(cart) => {
-               let pro = await ProductCart.findOne(
-                {
-                    where:{
-                        cartId:cart.id,
-                        productId:req.body.id    
-                    }});
-                
-               if(pro){
-                    await pro.update({
-                        quantity: req.body.quantity,
-                        totalPrice: req.body.totalPrice
-                    }
-                    )
-                    pro.save()
-                    res.status(200).send('Producto Modificado')
-                }
-            else{
-                res.status(500).send("Producto no encontrado");
-            }
-            }) 
-                    
-    }catch(error){
-        console.log(error);
-        throw new Error(error +' Error al cambiar datos del carrito')
+const deleteCartPro = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let cart = await Cart.findOne({ where: { userId: id } });
+    console.log(cart.id);
+    let pro = await ProductCart.findOne({
+      where: {
+        cartId: cart.id,
+        productId: req.body.id,
+      },
+    });
+    if (pro) {
+      pro.destroy();
+      res.status(200).send('Borrado con exito');
+    } else {
+      res.status(500).send('No se encontro un producto con ese ID');
     }
-}
-
-const deleteCart = async(req, res) => {
-    const {id} = req.params;
-
-    try {
-        let cart = await Cart.findOne({where:{userId:id}})
-        if(cart){
-            cart.destroy({where:{userId:id}})
-            res.status(200).send('Borrado con exito')                
-        }else{
-            res.status(500).send('No se encontro un usuario con ese ID')
-        }
-    } catch (error) {
-        throw new Error(error + ' error al eliminar')
-    }
-}
-
-const deleteCartPro = async(req, res) => {
-    const {id} = req.params;
-    try{
-        let cart = await Cart.findOne({where:{userId:id}});
-        console.log(cart.id)
-        let pro = await ProductCart.findOne(
-            {
-                where:{
-                    cartId:cart.id,
-                    productId:req.body.id    
-                }
-            }
-        );
-        if(pro){
-            pro.destroy()
-            res.status(200).send('Borrado con exito')                
-        }else{
-            res.status(500).send('No se encontro un producto con ese ID');
-        }
-    }catch(error){
-        throw new Error(error + ' error al eliminar un producto')
-    }
-}
+  } catch (error) {
+    throw new Error(error + ' error al eliminar un producto');
+  }
+};
 
 module.exports = {
-    cart,
-    updateCart,
-    deleteCart,
-    deleteCartPro
-}
+  getCart,
+  createCart,
+  updateCart,
+  deleteCart,
+  deleteCartPro,
+};
