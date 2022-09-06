ee8ee4937b7d668bb1edf9d2a3c9fc1019c81357
Braian Chavez <chavezbraian1999@gmail.com>
2022-09-06 12:11:19 -0300

    update post search finished

--- api/src/routes/Products/updateProduct.js
+++ api/src/routes/Products/updateProduct.js
@@ -5,13 +5,21 @@ const updateProduct = async (req, res) => {
     const { id } = req.params;
     let product = await Product.findByPk(id);
     if (product) {
-       await product.update(req.body)
+       const {name, description, price ,quantity ,image} = req.body
+      await product.update({
+        name: name,
+        description: description,
+        price: price,
+        quantity: product.quantity + quantity,
+        image: image
+       })
+       
       res.status(200).send('producto actualizado')
     } else {
       res.status(404).send("Producto no encontrado");
     }
   } catch (error) {
-    throw new Error(error + " error al actualizar un producto");
+    console.log(error);
   }
 };
 
